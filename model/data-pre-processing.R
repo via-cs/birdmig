26079261be26535c0213182129334647b495ae2d
ansellum <jbvogt@ucdavis.edu>
2024-03-03 14:19:09 -0800

    Organized R pre-pcoessing output
    Also changed lattitude and longitude to only California

--- model/data-pre-processing.R
+++ model/data-pre-processing.R
@@ -7,7 +7,7 @@ library(maptools)
 # MODIFY THESE PARAMETERS FOR EACH DATAFILE
 
 filename <- "CardellinaPusilla" # set filename
-e <- extent(-124, -69, 26, 48) # set study area extent
+e <- extent(-124, -114, 32, 42) # set study area extent
 
 # pipe GBIF data
 jt_raw <- read.csv(paste('data/GBIF_', filename, '.csv', sep = ""),
@@ -18,7 +18,6 @@ jt[,1] <- jt_raw$decimalLongitude
 jt[,2] <- jt_raw$decimalLatitude
 jt <- unique(jt) # xantusia without duplicates
 jt <- jt[complete.cases(jt),] # remove na's
-length_presences <- nrow(jt)
 colnames(jt) <- c('lon','lat')
 
 # download Bioclim features
@@ -32,17 +31,21 @@ bioclim.data <- getData(name = "worldclim",
 bioclim.data <- crop(bioclim.data, e*1.25)  # crop to bg point extent
 # write rasters to /data folder
 for (i in c(1:19)){
-  writeRaster(bioclim.data[[i]], paste('data/bclim/bclim', i, sep = ''),
+  writeRaster(bioclim.data[[i]], paste('data/bioclim/bclim', i, sep = ''),
               format = "ascii", overwrite=TRUE)
 }
 
 ## ------------------------------------------------------------------------
 
+# Trim jt
+jt_trim <- jt[sample(nrow(jt), 2000), ]
+length_presences <- nrow(jt_trim)
+
 # sample background points from a slightly wider extent
-bg <- randomPoints(bioclim.data[[1]], length_presences, ext=e, extf = 1.25) 
+bg <- randomPoints(bioclim.data[[1]], length_presences*2, ext=e, extf = 1.25)
 colnames(bg) <- c('lon','lat')
-train <- rbind(jt, bg)  # combine with presences
-pa_train <- c(rep(1, nrow(jt)), rep(0, nrow(bg))) # col of ones and zeros
+train <- rbind(jt_trim, bg)  # combine with presences
+pa_train <- c(rep(1, nrow(jt_trim)), rep(0, nrow(bg))) # col of ones and zeros
 train <- data.frame(cbind(CLASS=pa_train, train)) # final dataframe
  
 # create spatial points
@@ -51,7 +54,7 @@ train <- train[sample(nrow(train)),]
 class.pa <- data.frame(train[,1])
 colnames(class.pa) <- 'CLASS'
 dataMap.jt  <- SpatialPointsDataFrame(train[,c(2,3)], class.pa,
-                                      proj4string =crs)
+                                      proj4string = crs)
 # write as shp
 writeOGR(dataMap.jt,
          paste('data/shp/', filename, '.shp', sep = ""),
@@ -62,4 +65,6 @@ writeOGR(dataMap.jt,
 plot(bioclim.data[[1]], main='Bioclim 1')
 points(bg, col='red', pch = 16,cex=.3)
 points(jt, col='black', pch = 16,cex=.3)
+data("wrld_simpl")
 plot(wrld_simpl, add=TRUE, border='dark grey')
+
