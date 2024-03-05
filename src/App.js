7541fef3e9c72db31294e9c3f7f53f33dc470d0a
unknown <hanyun@ucdavis.edu>
2024-03-05 15:56:38 -0800

    Began some development for front end
    Began absolute basic work for front end to prepare for displaying bird info

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import axios from "axios";
 import logo from "./logo.svg";
 import "./App.css";
 
+// DEBUG content
+import distribution from "./DEBUG_dist.png";
+
 function App() {
+
   const [profileData, setProfileData] = useState(null);
+
+  // Read data from flask
   function getData() {
     axios({
       method: "GET",
@@ -15,6 +21,7 @@ function App() {
         setProfileData({
           profile_name: res.name,
           about_me: res.about,
+          distribData: distribution
         });
       })
       .catch((error) => {
@@ -23,20 +30,27 @@ function App() {
           console.log(error.response.status);
           console.log(error.response.headers);
         }
-      });
+        
+        setProfileData({
+            profile_name: "NULL Name",
+            about_me: "Check to make sure the backend is working.",
+            distribData: distribution
+          });
+      }
+    );
   }
-  //end of new line
 
+  // Export the main user interface for the application.
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
         <button onClick={getData}>Click me</button>
         {profileData && (
           <div>
             <p>Project name: {profileData.profile_name}</p>
             <p>Message: {profileData.about_me}</p>
+            <img src={profileData.distribData} className="Distribution Data" alt="distribution" />
           </div>
         )}
       </header>
