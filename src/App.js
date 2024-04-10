8434736078314682f06548c5af58917f8d504d80
unknown <hanyun@ucdavis.edu>
2024-04-10 01:39:47 -0700

    Integrated backend-frontend session storage
    Added session functionality between backend and front end of application, allowing for React requests to be "remembered" by Flask. Useful for remembering user requests to send data to front end after training model.
    
    TODO: encapsulate session cookies from other front-end utilities. Include model training to fully integrate back and front ends of project.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-/** > > > IMPORTANT: < < <
- * 
- * THIS IS A DUMMIED-OUT APP.JS SOLELY FOR DEBUGGING SESSIONS.
- * THIS SHOULD NEVER REPLACE ACTUAL WORK DONE FOR THE FRONT END.
-*/
-
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
-import BirdInfo from "./components/BirdInfo";
-import SDMChart from "./components/SDMChart";
-import TimeSeries from "./components/TimeSeries";
+import { CookiesProvider, useCookies } from "react-cookie"
 
 function App() {
 
+  axios.create({withCredentials:true});
+  // override axios defaults to enable credentials for flask sessions.
+  axios.defaults.withCredentials  = true
+
+  // Allow the app to use cookies, for utility with backend request memory.
+  const [cookies, setCookie] = useCookies(['user'])
+
+  // Set cookies on request. TODO: find more graceful solution to set cookies.
+  function onRequest() {
+    setCookie('user', 1)
+  }
+
   const [io_result, setResult] = useState(null);
   const baseUrl = 'http://localhost:5000';
-  
+
+  useEffect(() => {
+    setResult("Test");
+  }, []);
+
+  // Generic, quick and dirty function for getting data.
   function getData() {
-    setLoading(true);
-    setError(null);
-  
-    axios.get(`${baseUrl}/model-input`)
+    axios.get(`${baseUrl}/model-output`)
       .then(response => {
         setResult(response.msg);
       })
@@ -32,13 +38,9 @@ function App() {
 
   // Generic, quick and dirty function for sending data.
   function sendData(birdName) {
-    axios.post(`${baseUrl}/model_input`, )
+    axios.post(`${baseUrl}/model-input/${birdName}`, birdName)
     .then(({data}) => {
-      setProfileData({
-        profile_name: data.name,
-        about_me: data.about,
-        data: data.output
-      })
+      setResult(data.msg)
     })
     .catch((error) => {
       if(error.response) {
@@ -49,8 +51,17 @@ function App() {
   
   return (
     <div className="App">
-      <button/>
-      <button/>
+      <CookiesProvider>
+        <button onClick={()=>getData()}>GET Data</button>
+        <button onClick={()=>sendData('Bird 1')}>SEND Bird 1</button>
+        <button onClick={()=>sendData('Bird 2')}>SEND Bird 2</button>
+
+        { io_result ? (
+          <p>Response: {io_result.msg}</p>
+        ) : (
+          <p>Try to select a bird!</p>
+        )}
+      </CookiesProvider>
     </div>
   );
 }
