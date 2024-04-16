f933e4cf81ec181d4f4bc2e2c95cd963fc401e59
Mihika Krishna <krishna.mihika@gmail.com>
2024-04-16 15:30:59 -0700

    Create endpoint for migration images

--- backend/app/base.py
+++ backend/app/base.py
@@ -1,45 +1,59 @@
-from flask import Flask, jsonify, request, redirect, url_for
+import os
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS, cross_origin
-# TEST for alpha.
+from jinja2 import TemplateNotFound
+
+# Importing for potential image manipulation, not used directly in provided code.
 from PIL import Image
 from io import BytesIO
 import base64
 
-api = Flask(__name__)
-cors = CORS(api)
+api = Flask(__name__, template_folder='../templates')
+CORS(api)
 api.config['CORS_HEADERS'] = 'Content-Type'
 
+# Example data for demonstration purposes.
 DEMO_bird_data = {
-  "Bird 1": {
-      "info": "Generic info for unique bird 1",
-      "sdmData": [{"x": 1, "y": 300}, {"x": 2, "y": 600}, {"x": 3, "y": 800}],
-      "timeSeriesData": {
-          "precipitation": [300, 400, 500],
-          "climate": [30, 40, 50],
-          "temperature": [60, 70, 80],
-      },
-  },
-  "Bird 2": {
-      "info": "Generic info for different bird 2",
-      "sdmData": [{"x": 1, "y": 100}, {"x": 2, "y": 200}, {"x": 3, "y": 500}],
-      "timeSeriesData": {
+    "Bird 1": {
+        "info": "Generic info for unique bird 1",
+        "sdmData": [{"x": 1, "y": 300}, {"x": 2, "y": 600}, {"x": 3, "y": 800}],
+        "timeSeriesData": {
             "precipitation": [300, 400, 500],
             "climate": [30, 40, 50],
             "temperature": [60, 70, 80],
-      } 
-  }
+        },
+    },
+    "Bird 2": {
+        "info": "Generic info for different bird 2",
+        "sdmData": [{"x": 1, "y": 100}, {"x": 2, "y": 200}, {"x": 3, "y": 500}],
+        "timeSeriesData": {
+            "precipitation": [300, 400, 500],
+            "climate": [30, 40, 50],
+            "temperature": [60, 70, 80],
+        }
+    }
 }
 
 @api.route('/bird-data/<bird_name>')
 @cross_origin()
 def get_bird_data(bird_name):
+    if bird_name in DEMO_bird_data:
+        return jsonify(DEMO_bird_data[bird_name])
+    else:
+        print(f"Requested bird: [{bird_name}] is not cached.")
+        return jsonify({"error": "Invalid bird"}), 404
   
-  # Using example data
-  if bird_name in DEMO_bird_data:
-    return jsonify(DEMO_bird_data[bird_name])
-  else :
-    print("Requested bird: [" + bird_name + "] is not cached.")
-    return jsonify({"error": "Invalid bird"}), 404
+@api.route('/migration_images/<filename>')
+def migration_image(filename):
+    print(f"Attempting to render: {filename}")
+    try:
+        if not filename.endswith('.html'):
+            filename += '.html'
+        return render_template(filename)
+    except TemplateNotFound as e:
+        print(f"Error: {e}")
+        return 'Template not found', 404
+
 
 # Running app
 if __name__ == '__main__':
