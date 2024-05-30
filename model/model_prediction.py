c2836edca09f1014b41f619ad27ae5d35e77158d
ansellum <jbvogt@ucdavis.edu>
2024-05-29 21:21:27 -0700

    Updated predictions

--- model/model_prediction.py
+++ model/model_prediction.py
@@ -6,28 +6,29 @@ from pyimpute import impute
 import pickle
 import rasterio
 from matplotlib.pylab import plt
+import rioxarray
 
 species_list = ['Anser_albifrons', 'Haliaeetus_leucocephalus', 'Numenius_americanus', 'Numenius_phaeopus', 'Setophaga_striata']
 year_list = list(range(2015, 2101))
 ssp_list = ["ssp126", "ssp245", "ssp370", "ssp585"]
 
 def make_prediction(species, ssp, year):
-    raster_features = sorted(glob.glob('data/MIROC6/' + ssp + '/' + str(year) + '/*.asc'))
+    raster_features = sorted(glob.glob('data/MIROC6/' + ssp + '/' + str(year) + '/*.tif'))
 
     target_xs, raster_info = load_targets(raster_features)
 
     pickle_filename = "outputs/models/" + species + '_et_classifier_model.pkl'
     model = pickle.load(open(pickle_filename, 'rb'))
 
-    os.makedirs('outputs/tiff-images/' + species + '/' + ssp + '/' + str(year), exist_ok=True)
+    os.makedirs('outputs/tiff-images/' + species + '/' + ssp + '/' + str(year) + '/3587', exist_ok=True)
     impute(target_xs, model, raster_info, outdir='outputs/tiff-images/' + species + '/' + ssp + '/' + str(year),
             class_prob=True, certainty=True)
                 
-def convert_tiffs(species, ssp, year):
+def create_pngs(species, ssp, year):
     distr = rasterio.open('outputs/tiff-images/' + species + '/' + ssp + '/' + str(year) + '/probability_1.0.tif').read(1)
 
     fig = plt.figure(frameon=False)
-    fig.set_size_inches(3,4)
+    fig.set_size_inches(4,4)
     ax = plt.Axes(fig, [0., 0., 1., 1.])
     ax.set_axis_off()
     fig.add_axes(ax)
@@ -43,6 +44,10 @@ for species in tqdm(species_list, desc="Species"):
         for ssp in tqdm(ssp_list, desc="ssp"):
             for year in tqdm(year_list, desc="Years"):
                 make_prediction(species, ssp, year)
-                convert_tiffs(species, ssp, year)
+                create_pngs(species, ssp, year)
+
+                rds = rioxarray.open_rasterio('outputs/tiff-images/' + species + '/' + ssp + '/' + str(year) + '/probability_1.0.tif')
+                rds_3587 = rds.rio.reproject("EPSG:3587")
+                rds_3587.rio.to_raster('outputs/tiff-images/' + species + '/' + ssp + '/' + str(year) + '/3587/probability_1.0.tif')
 
                 
