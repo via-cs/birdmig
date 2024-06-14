62ff8ac42f61a9dd331b52ac61a1ad8536b91c40
Mihika Krishna <krishna.mihika@gmail.com>
2024-06-13 21:40:31 -0700

    Fix failing frontend unit tests

--- src/components/PredictionControls.test.js
+++ src/components/PredictionControls.test.js
@@ -1,15 +1,14 @@
-// PredictionControls.test.js
-
 import React from "react";
 import { render, fireEvent, screen } from "@testing-library/react";
 import PredictionControls from "./PredictionControls";
 
 describe("PredictionControls Component", () => {
-	test("updates CO2 emission scenario and calls onPredictionUpdated", () => {
+	test("updates CO2 emission scenario and calls onPredictionUpdated", async () => {
 		const handlePredictionUpdated = jest.fn();
 		render(<PredictionControls onPredictionUpdated={handlePredictionUpdated} />);
 
-		const initialButton = screen.getByRole("button", { name: "ssp126" });
+		// Use the correct name as displayed in the UI
+		const initialButton = screen.getByRole("button", { name: "SSP1-2.6" });
 		fireEvent.click(initialButton);
 		expect(handlePredictionUpdated).toHaveBeenCalledWith(2021, "ssp126");
 	});
